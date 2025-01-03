b473c3aae22f29f7f89ff3b2443c6ac9837feb08
KvN Roualec <kevin.roualec@hotmail.fr>
2025-01-03 18:40:45 +0100

    Update API.py

--- API.py
+++ API.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from transformers import RobertaTokenizer, TFRobertaForSequenceClassification
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 
@@ -23,15 +23,32 @@ def read_root():
 
 @app.post("/predict/")
 def predict(request: PredictionRequest):
-    tokenizer, model = load_roberta_model_and_tokenizer("./fine_tuned_roberta")
-    inputs = tokenizer(request.text, return_tensors="tf", max_length=64, padding="max_length", truncation=True)
-    logits = model(inputs).logits
-    probabilities = tf.nn.softmax(logits, axis=-1).numpy()[0]
-    predicted_label = tf.argmax(probabilities).numpy()
-    return {"text": request.text, "predicted_label": int(predicted_label), "confidence": float(probabilities[predicted_label])}
+    try:
+        # Validation de l'entrée
+        if not request.text:
+            raise HTTPException(status_code=400, detail="Le texte ne peut pas être vide.")
+        
+        tokenizer, model = load_roberta_model_and_tokenizer("./fine_tuned_roberta")
+        inputs = tokenizer(request.text, return_tensors="tf", max_length=64, padding="max_length", truncation=True)
+        
+        # Vérification des entrées tokenisées
+        if inputs.get('input_ids') is None:
+            raise HTTPException(status_code=500, detail="Erreur lors de la tokenisation.")
+        
+        logits = model(inputs).logits
+        probabilities = tf.nn.softmax(logits, axis=-1).numpy()[0]
+        predicted_label = tf.argmax(probabilities).numpy()
+
+        return {
+            "text": request.text,
+            "predicted_label": int(predicted_label),
+            "confidence": float(probabilities[predicted_label])
+        }
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 if __name__ == "__main__":
-    # Lancer le serveur FastAPI
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
