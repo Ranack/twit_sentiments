9a704a00c1a2cb7bf8cfc4a8f60ce17b05a4c3c2
KvN Roualec <kevin.roualec@hotmail.fr>
2025-01-07 15:23:31 +0100

    Update App.py

--- App.py
+++ App.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import requests
 
-# Définir l'URL de votre API FastAPI en local
-API_URL = "http://127.0.0.1:5000/predict/"
+# Définir l'URL de votre API FastAPI sur Azure
+API_URL = "https://twisentiment-v2.azurewebsites.net/predict/"
 
 # Interface Streamlit
 st.title("Test de Classification de Texte avec RoBERTa Fine-Tuné")
@@ -12,25 +12,25 @@ text_input = st.text_area("Entrez le texte à analyser", "")
 
 # Si le bouton "Prédire" est cliqué
 if st.button("Prédire"):
-
     if text_input.strip() != "":
         # Créer la payload pour la requête
         payload = {"text": text_input}
 
-        # Envoyer la requête POST à l'API
-        try:
-            response = requests.post(API_URL, json=payload)
-            
-            # Vérifier la réponse de l'API
-            if response.status_code == 200:
-                result = response.json()
-                st.subheader("Résultats de la prédiction")
-                st.write(f"Texte : {result['text']}")
-                st.write(f"Label prédit : {result['predicted_label']}")
-                st.write(f"Confiance : {result['confidence']:.2f}")
-            else:
-                st.error("Erreur dans la réponse de l'API")
-        except Exception as e:
-            st.error(f"Erreur lors de la connexion à l'API : {e}")
+        # Afficher un message de chargement
+        with st.spinner("Envoi de la requête à l'API..."):
+            try:
+                response = requests.post(API_URL, json=payload)
+
+                # Vérifier la réponse de l'API
+                if response.status_code == 200:
+                    result = response.json()
+                    st.subheader("Résultats de la prédiction")
+                    st.write(f"Texte : {result['text']}")
+                    st.write(f"Label prédit : {result['predicted_label']}")
+                    st.write(f"Confiance : {result['confidence']:.2f}")
+                else:
+                    st.error("Erreur dans la réponse de l'API")
+            except Exception as e:
+                st.error(f"Erreur lors de la connexion à l'API : {e}")
     else:
         st.warning("Veuillez entrer un texte à analyser")
